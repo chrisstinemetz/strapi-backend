60b02e76ca85c65dd85666557a5b430b97a2f58a
Chris Stinemetz <chris.stinemetz@gmail.com>
2021-02-14 14:33:29 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b9c755f55ec956849b3d45102ed8c63e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '500d5d3b-63cb-4588-8b63-b2e223bb3b64'
+};
